edb007601cdacb91285fbf71b6ae7c77c617e499
sarleone <sarah.m.leone@gmail.com>
2018-12-18 13:04:36 -0600

    updated package.json file

---
+++ build/precache-manifest.908b2f47a133d78d13e874261430a48c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "621d98cfb5eb2c54d040",
+    "url": "/static/js/main.621d98cf.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "621d98cfb5eb2c54d040",
+    "url": "/static/css/main.92335d12.chunk.css"
+  },
+  {
+    "revision": "df726eda2ba2f806093484cff6d58fe7",
+    "url": "/index.html"
+  }
+];
